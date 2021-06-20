669b978945561f9e35dd79e4afdd528ecb51a459
smithmule <52617285+smithmule@users.noreply.github.com>
2021-06-20 18:59:09 +1000

    Create ball.py

---
+++ ball.py
@@ -0,0 +1,54 @@
+##pygame tutorial to bounce a ball around the screen
+#import the needed modules
+import sys, pygame, time
+
+#call to set up some crap
+pygame.init()
+
+#set screen size variables
+size = width, height = 960, 540
+
+#set x,y speed vector array table list thingy variable
+speed = [2, 2]
+
+#make a colour
+black = 0, 0, 0
+
+#create the window
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('ball tutorial')
+
+#load ball image into a variable
+ball = pygame.image.load("intro_ball.gif")
+
+#create a sprite using ball variable??
+ballrect = ball.get_rect()
+
+#main game loop
+while 1:
+    
+    #set fps
+    pygame.time.Clock().tick(60)
+    
+    #check for exit button press
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    #move the sprite
+    ballrect = ballrect.move(speed)
+    
+    #check for x limits
+    if ballrect.left < 0 or ballrect.right > width:
+        #reverse x direction
+        speed[0] = -speed[0]
+    #check for y limits
+    if ballrect.top < 0 or ballrect.bottom > height:
+        #reverse y direction
+        speed[1] = -speed[1]
+
+    #clear screen
+    screen.fill(black)
+    #put the sprite into fram buffer??
+    screen.blit(ball, ballrect)
+    #draw the buffer to the screen??
+    pygame.display.flip()
